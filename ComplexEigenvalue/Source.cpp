417d29ca0ae94a3d97559c3dbd06f160d6de2965
Ryota Majima <ryota.0571@gmail.com>
2015-12-08 17:08:25 +0900

    [modify] params

--- ComplexEigenvalue/Source.cpp
+++ ComplexEigenvalue/Source.cpp
@@ -29,11 +29,11 @@ inline fftw_complex* fftwcast(Complex* f){ return reinterpret_cast<fftw_complex*
 
 const double X_BEGIN = -5.0, X_END = 25.0; //Œn‚Ì—¼’[
 const double L = X_END - X_BEGIN; //‹óŠÔ•
-const int N = 256; //‹óŠÔ•ªŠ„”
+const int N = 512; //‹óŠÔ•ªŠ„”
 const double DELTA_X = L / N;
 
-const double T_END = 100; //I—¹Žž
-const int TN = T_END * 10; //ŽžŠÔ•ªŠ„”
+const double T_END = 50; //I—¹Žž
+const int TN = T_END * 2; //ŽžŠÔ•ªŠ„”
 const double dt = T_END / TN; //ŽžŠÔ‚Ý•
 
 const double S = 0.9; //”g‘©‚Ì•
@@ -46,7 +46,7 @@ const int EN_real = 200;
 const double dE_real = (E_END_real - E_BEGIN_real) / EN_real;
 
 const double E_BEGIN_imag = -0.02, E_END_imag = 0.02;
-const int EN_imag = 400;
+const int EN_imag = 200;
 const double dE_imag = (E_END_imag - E_BEGIN_imag) / EN_imag;
 
 double i2x(int i){
